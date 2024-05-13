610b311ea0f043200c64d055e48cd093fae507b5
gary <goldmine@163.com>
2024-05-13 11:35:41 +0800

    update vue and prisma in lsp

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,22 +1,26 @@
 require("mason").setup({
-  ui = {
-      icons = {
-          package_installed = "✓",
-          package_pending = "➜",
-          package_uninstalled = "✗"
-      }
-  }
+	ui = {
+		icons = {
+			package_installed = "✓",
+			package_pending = "➜",
+			package_uninstalled = "✗",
+		},
+	},
 })
 
 require("mason-lspconfig").setup({
-  -- 确保安装，根据需要填写
-  ensure_installed = {
-    "lua_ls",
-  },
+	-- 确保安装，根据需要填写
+	ensure_installed = {
+		"lua_ls",
+	},
 })
 
-local capabilities = require('cmp_nvim_lsp').default_capabilities()
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-require("lspconfig").lua_ls.setup {
-  capabilities = capabilities,
-}
+require("lspconfig").lua_ls.setup({
+	capabilities = capabilities,
+})
+
+require("lspconfig").tsserver.setup({})
+require("lspconfig").vuels.setup({})
+require("lspconfig").prismals.setup({})
